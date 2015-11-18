0e8fab9a702d92b571e420e59c84c95395db73c8
edeno <edeno@bu.edu>
2015-11-18 16:24:32 -0500

    Remove comments

--- makePresentation.sh
+++ makePresentation.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Make presentation
 pandoc src/presentation.md \
-  --to revealjs \ # Use revealjs for presentation
-  --variable revealjs-url="public/revealjs" \ #revealjs directory
-  --variable slideNumber="true" \ # Add slide numbers
-  --variable transition="none" \ # Turn off slide transitions
+  --to revealjs \
+  --variable revealjs-url="public/revealjs" \
+  --variable slideNumber="true" \
+  --variable transition="none" \
   --normalize \
   --standalone \
   --self-contained \
